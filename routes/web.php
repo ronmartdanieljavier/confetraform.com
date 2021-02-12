9bfa093c60c2901aaac806963bdb74d182e48d48
ronmartdanieljavier <ronmartdanieljavier@gmail.com>
2021-02-12 14:40:11 +1100

    Create sample templates

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/dashboard', function () {
+    return view('dashboard_page');
+});
+Route::get('/sample', function () {
+    return view('sample_page');
+});
 
 Auth::routes();
 
